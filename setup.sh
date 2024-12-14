61974af82c71f74326ca28b0c806c69ffcf276fa
woudch <wm.it.official@gmail.com>
2024-12-14 09:53:55 +0300

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+echo "SCRIPT ACTUALLY IN TESTING AND MAYBE IT HAS A LOT OF BUGS"
+echo "YOU NEED SUDO TO USE THIS!"
+sleep 3
+
+YAY -S screenfetch-git
+sudo pacman -S --noconfirm neofetch xorg xorg-xinit sddm sddm-kcm mesa
+sudo systemctl enable sddm
+sudo pacman -S --noconfirm i3-wm i3 i3status dmenu polybar 
+sudo localectl set-locale ru_RU.UTF-8
+sudo localectl set-x11-keymap --no-convert us,ru pc105 "" grp:alt_shift_toggle
+sudo pacman -S --noconfirm doas bsd-games kitty 
+cd ~/
+git clone https://aur.archlinux.org/packages/yay
+cd ~/yay 
+makepkg -si
+cd ..
+yay -S google-chrome
+cd PigOS
+sudo pacman -S --noconfirm virtualbox-host-modules-arch
+sudo pacman -S --noconfirm virtualbox
+sudo pacman -S --noconfirm firefox
+sudo pacman -S --noconfirm jdk-openjdk
+sudo pacman -S --noconfirm obs-studio ncdu vim gimp mplayer mpg123
+sudo pacman -S --noconfirm zip unzip imagemagick scrot wget curl
+
+modprobe vboxdrv 
+
+# кириллический шрифт для терминала еще в тестинге так что извините если не работает 0_0
+
+echo "FONT=cyr-sun16" >> /etc/vconsole.conf
+sleep 5
+
+cat ./pig
+
+echo "installation complete"
+sleep 2
+echo "now reboot pc"
+# - зачем нужно ставить PigOS?
+# - там есть интернет. 
+
+
+
+
